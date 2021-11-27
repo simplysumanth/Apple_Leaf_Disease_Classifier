e64b88a75430acc238a6f657f48f12c2eedbe333
simplysumanth <harisumanth1998@gmail.com>
2021-11-28 01:52:43 +0530

    modified streamlit app

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import pandas as pd
 import numpy as np
 from fastai.vision.all import *
 
+import base64
+
 from sklearn.metrics import roc_auc_score
 
 VOCAB_LIST = ['healthy', 'multiple_diseases', 'rust', 'scab']
 
-sts.title("Plant Pathology Classifier")
+sts.title("Apple Leaf Disease Detector")
+
+sts.text("Submit a apple leaf image and the app will detect its disease(if any)")
+
 
 file = sts.file_uploader("Pick a file")
 
@@ -38,10 +43,13 @@ def scab_roc_Auc(*args):
     return total_error_metric(*args, labels = [3])
 
 if file is not None:
-    if sts.button("Classify"):
+    if sts.button("Detect"):
 
         learn_inf = load_learner(model_path/"base_model_export.pkl", cpu=True)
 
         output = learn_inf.predict(file.read())[0]
 
-        sts.header(f"The Apply tree is : {str(output).capitalize()}")
+        if str(output) == "healthy":
+            sts.header(f"The Apple Leaf is : {str(output).capitalize()}")
+        else:
+            sts.header(f"The Apple Leaf has : {str(output).capitalize()} disease")
